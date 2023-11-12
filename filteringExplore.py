2888f8b03dc5ec2d1f78f3025d88b7fa773a9cbe
Spicer <spicer.bak.frf@gmail.com>
2023-11-12 09:21:00 -0500

    created plot of imagery background with survey track on it

--- filteringExplore.py
+++ filteringExplore.py
@@ -1,17 +1,71 @@
-from testbedutils imoprt geoprocess
-from minio import Minio
-import wget
+import datetime as DT
 import os
+from matplotlib import pyplot as plt
+import h5py
+import rasterio
+import wget
+from rasterio import plot as rplt
+from testbedutils import geoprocess
 
-fname = '/data'
 
-#https://pratiman-91.github.io/2020/06/30/Plotting-GeoTIFF-in-python.html
+def getArgusImagery(dateOfInterest, ofName=None, imageType='timex', verbose=True):
+    # client = Minio("coastalimaging.erdc.dren.mil")
+    # ## now lets find what files are around
+    # objects = client.list_objects('FrfTower', prefix="Processed/alignedObliques/c1", recursive=True,)
+    baseURL = "https://coastalimaging.erdc.dren.mil/FrfTower/Processed/Orthophotos/cxgeo/"
+    fldr = dateOfInterest.strftime("%Y_%m_%d")
+    fname = f'{dateOfInterest.strftime("%Y%m%dT%H%M%SZ")}.FrfTower.cxgeo.{imageType}.tif'
+    wgetURL = os.path.join(baseURL, fldr, fname)
+    if ofName is None:
+        ofName = os.path.join(os.getcwd(), os.path.basename(wgetURL))
+    wget.download(wgetURL, ofName)
+    if verbose: print(f"retrieved {ofName}")
+    return ofName
 
-# convert to FRF
-# if makePos is True and 'ncdu' in baseZipFiles:
-coords = geoprocess.FRFcoord(lon_out[idxDataToSave], lat_out[idxDataToSave])
+def unpackYellowfinCombinedRaw(fname):
+    data = {}
+    with h5py.File(fname, 'r') as hf:
+        for var in ['time', 'longitude', 'latitude', 'elevation', 'fix_quality_GNSS', 'sonar_smooth_depth',
+                    'sonar_smooth_confidence', 'sonar_instant_depth', 'sonar_instant_depth', 'sonar_instant_confidence',
+                    'sonar_backscatter_out', 'bad_lat', 'bad_lon', 'xFRF', 'yFRF', 'Profile_number']:
+            data[var] = hf.get(var)[:]
+    return data
+
+def plotPlanViewOnArgus(data, geoTifName):
+    """plots a survey path over a geotiff at the FRF (assumes NC stateplane)
+    Args:
+        data: this is a dictionary of data loaded with keys of 'longitude', 'latitude', 'elevation'
+        geoTifName: this is a filenamepath of a geotiff file over which elevation and path data are to be overlayed
+
+    """
+    coords = geoprocess.FRFcoord(data['longitude'], data['latitude'])s
+    timex = rasterio.open(geoTifName)
+    # array = timex.read()  # for reference, this pulls the image data out of the geotiff object
+    ## now make plot
+    plt.figure(figsize=(14,10))
+    ax1 = plt.subplot()
+    aa = rplt.show(timex, ax=ax1)
+    a = ax1.scatter(coords['StateplaneE'], coords['StateplaneN'], c=data['elevation'], vmin=-8)
+    cbar = plt.colorbar(a)
+    cbar.set_label('depths')
+    ax1.set_xlabel('NC stateplane Easting')
+    ax1.set_ylabel('NC stateplane Northing')
+    plt.savefig('Overview_on_Argus.png')
+    plt.close()
+
+yellowFinDatafname = '/data/yellowfin/20231109/20231109_totalCombinedRawData.h5'
+data = unpackYellowfinCombinedRaw(yellowFinDatafname)
+# convert to all coords
 pierStart = geoprocess.FRFcoord(0, 515, coordType='FRF')
 pierEnd = geoprocess.FRFcoord(534, 515, coordType='FRF')
+
+#now get imagery
+dateOfInterest = DT.datetime(2023, 11, 9, 13, 0, 0)  # "20231109T120000Z"
+imageType = 'timex'
+# a = getArgusImagery(dateOfInterest)
+geoTifName = "/home/spike/repos/SWACSS/20231109T130000Z.FrfTower.cxgeo.timex.tif"
+# https://pratiman-91.github.io/2020/06/30/Plotting-GeoTIFF-in-python.html
+plotPlanViewOnArgus(data, geoTifName)
 
 ## isolate and focus on one FRF line profile
 minloc = 1270
@@ -23,7 +77,8 @@ passedBathy = yellowfinLib.butter_lowpass_filter(elevation_out[idxDataToSave][lo
 plt.figure(figsize=(12, 8));
 plt.subplot(211)
 plt.title('plan view of survey')
-plt.scatter(coords['xFRF'], coords['yFRF'], c=elevation_out[idxDataToSave], vmax=-1)  # time_out[idxDataToSave])  #
+plt.scatter(coords['xFRF'], coords['yFRF'], c=elevation_out[idxDataToSave],
+            vmax=-1)  # time_out[idxDataToSave])  #
 cbar = plt.colorbar()
 cbar.set_label('depth')
 plt.subplot(212)
@@ -40,5 +95,3 @@ plt.xlabel('xFRF')
 plt.ylabel('elevation NAVD88[m]')
 plt.tight_layout()
 plt.savefig(os.path.join(plotDir, 'singleProfile.png'))
-
-
