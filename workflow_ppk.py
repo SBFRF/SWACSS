2a7533c514c41e0a25c0671b1590387898c40ab2
Kara Koetje <kkoetje@users.noreply.github.com>
2024-12-07 20:29:19 +0000

    Add prompts to user to create metadata YAML file (#13)
    * added logic to first look to see if the profile line netcdf has already been created, and if so, skip the transect line selection step
    
    * Added function to create `mission summary yaml` file, call in workflow
    
    * Added function to create `mission failure YAML`  file
    ---------
    
    Co-authored-by: SBFRF <spicer.bak.frf@gmail.com>

--- workflow_ppk.py
+++ workflow_ppk.py
@@ -18,7 +18,9 @@ import tqdm
 from testbedutils import geoprocess
 import argparse, logging
 
-__version__ = 0.2
+from mission_yaml_files import make_summary_yaml, make_failure_yaml
+
+__version__ = 0.3
 def parse_args(__version__):
     parser = argparse.ArgumentParser(f"PPK processing for yellowfin (V{__version__})", add_help=True)
     # datadir, geoid, makePos = True, verbose = 1
@@ -415,6 +417,13 @@ def main(datadir, geoid, makePos=True, verbose=2, sonar_method='default', rtklib
             hf.create_dataset('xFRF', data=coords['xFRF'])
             hf.create_dataset('yFRF', data=coords['yFRF'])
             hf.create_dataset('Profile_number', data=data['Profile_number'])
+
+
+    # Make mission summary YAML based on user prompted inputs, write to datadir
+    make_summary_yaml(datadir)
+
+    # Make mission failure YAML based on user prompted inputs, write to datadir
+    make_failure_yaml(datadir)
 
 
 if __name__ == "__main__":
